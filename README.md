17349254bca1dee42d9c4253882660aa01e7ce10
Filip Dutescu <filip.dutescu@gmail.com>
2021-05-19 21:57:21 +0300

    init repo

---
+++ README.md
@@ -0,0 +1,10 @@
+# Time series project
+
+This repo represents the code part of the team project made with @SebastianDochia,
+for our Time series course, from our Bachelor's Degree program.
+
+## Awknoledgements
+
+* Dataset was kindly provided by Bhaskar Tripathi on Kaggle, as
+[Bitcoin Time Series - All Time](https://www.kaggle.com/bhaskartripathi/bitcoin-time-series-all-time).
+
---
+++ main.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from statsmodels.tsa.stattools import adfuller
+
+def read_csv(filename: str, original_cols: list, new_cols: list = None):
+    csv = pd.read_csv(filename, usecols=original_cols)
+
+    if new_cols is not None:
+        cols_dict = { og_col: new_col for (og_col, new_col) in zip(original_cols, new_cols) }
+        csv.rename(columns=cols_dict, inplace=True)
+
+
+    return csv
+
+def basic_statistics(data: pd.DataFrame, with_graph: bool = False):
+    print(data.info())
+    print(data.describe())
+    print()
+
+    if with_graph is True:
+        data.plot(x='date', y='price', rot=-45)
+        plt.show()
+        data.plot(x='date', y='price', kind='hist', rot=-45)
+        plt.show()
+
+def adfuller_test(series: pd.DataFrame):
+    test_result = adfuller(series, autolag='AIC')
+
+    print('ADF Statistic: \t%f' % test_result[0])
+    print('p-value: \t%f' % test_result[1])
+    print('\nCritical values:')
+    for (key, value) in test_result[4].items():
+        print('\t%s, %f' % (key, value))
+
+    print()
+
+def main():
+    original_cols = [
+        'Date',
+        'Close',
+        'MarketCap',
+        'Difficulty',
+        'MinersRevenue',
+        'TransactionFees_BTC',
+        'Total_Transactions_Per_Day',
+        'NASDAQ Composite',
+        'Dow Jones Industrial Avg',
+        'S&P 500',
+        'Google Trends Interest',
+        'Breakeven Inflation Rate',
+    ]
+    cols = [
+        'date',
+        'price',
+        'market_cap',
+        'difficulty',
+        'miners_revenue',
+        'transaction_fees',
+        'total_transactions_per_day',
+        'nasdaq_composite',
+        'dji_avg',
+        'sp500',
+        'google_trends',
+        'inflation_rate',
+    ]
+    source_file = 'block_chain_ts.csv'
+    btc_data = read_csv(source_file, original_cols, cols)
+
+    basic_statistics(btc_data, with_graph=False)
+
+    for col in cols:
+        if col != 'date':
+            print('\t==== ADF for %s ====\n' % col)
+            adfuller_test(btc_data[col])
+
+if __name__ == '__main__':
+    main()
+
